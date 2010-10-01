4f6b6a1118b2665c65222a1a507f828e8dc8778b
John Steele <programjsteele@gmail.com>
2010-10-01 10:25:50 -0700

    A standard definition file with major, minor, and number of char devices.

---
+++ src/read_write.h
@@ -0,0 +1,23 @@
+
+#ifndef _READ_WRITE_H_
+#define _READ_WRITE_H_
+
+#ifndef _MY_MAJOR
+#define _MY_MAJOR 0
+#endif
+
+#ifndef _MY_MINOR
+#define _MY_MINOR 0
+#endif  
+
+#ifndef NUM_DEVICES
+#define NUM_DEVICES 1
+#endif
+
+extern int my_major;
+extern int my_minor;
+extern int num_devices; 
+
+
+#endif /* _READ_WRITE_H_
+
